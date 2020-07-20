1306b107324c2457f7db28112b2a475ab007ba1b
RustyYato <krishna.sd.2012@gmail.com>
2020-07-20 13:19:34 -0400

    started work on parsing the block expr of function

--- purust-macro/src/encode.rs
+++ purust-macro/src/encode.rs
@@ -22,17 +22,37 @@ struct EnumVariant {
     generics: Vec<syn::Type>,
 }
 
+// let vis = &item.vis;
+// let ident = &sig.ident;
+// let unsafety = &sig.unsafety;
+// let ret = &sig.output;
+// let block = &*item.block;
+
+struct ReturnType {
+    ident: syn::Ident,
+    ty: syn::Type,
+}
+
 struct Signature {
+    vis: syn::Visibility,
+    ident: syn::Ident,
+    unsafety: Option<syn::Token![unsafe]>,
+    output: Vec<ReturnType>
     
 }
 
-struct Function {
+enum Stmt {
 
+}
+
+struct Function {
+    sig: Signature,
+    block: Vec<Stmt>,
 }
 
 enum Item {
     Enum(Vec<EnumVariant>),
-    Func(Vec<Function>)
+    Func(Function)
 }
 
 struct Items {
@@ -93,26 +113,59 @@ impl syn::visit::Visit<'_> for Items {
         if sig.constness.is_some() {
             errors = quote!(#errors compile_error!("type functions cannot be marked `const`"););
         }
-        
         if sig.asyncness.is_some() {
             errors = quote!(#errors compile_error!("type functions cannot be marked `async`"););
         }
-
         if sig.abi.is_some() {
             errors = quote!(#errors compile_error!("type functions cannot have a custom abi"););
         }
-
         if sig.variadic.is_some() {
             errors = quote!(#errors compile_error!("type functions cannot be variadic"););
         }
-
         if !sig.generics.params.is_empty() && sig.generics.params.len() != sig.generics.lifetimes().count() {
             errors = quote!(#errors compile_error!("type functions may only be generic over lifetimes"););
         }
-
         if !errors.is_empty() {
             self.errors = errors;
             return;
         }
+
+        let stmts = parse(&mut self.errors, &block.stmts);
+
+        let func = Function {
+            block: stmts,
+            sig: Signature {
+                vis: vis.clone(),
+                ident: ident.clone(),
+                unsafety: unsafety.clone(),
+                output: match ret {
+                    syn::ReturnType::Default => Vec::new(),
+                    syn::ReturnType::Type(_, ty) => vec![ReturnType {
+                        ident: syn::Ident::new("Output", proc_macro2::Span::call_site()),
+                        ty: syn::Type::clone(ty),
+                    }]
+                },
+            },
+        };
+
+        self.items.push(Item::Func(func));
     }
+}
+
+fn parse(errors: &mut TokenStream, stmts: &[syn::Stmt]) -> Vec<Stmt> {
+    let stmts_iter = stmts.iter();
+    let mut stmts = Vec::new();
+    stmts_iter.for_each(|stmt| match stmt {
+        syn::Stmt::Local(local) => {
+            *errors = quote!(#errors compile_error!("let bindings are unimplemented"););
+        },
+        syn::Stmt::Item(_) => {
+            *errors = quote!(#errors compile_error!("items cannot be used inside of type functions"););
+        },
+        | syn::Stmt::Expr(expr)
+        | syn::Stmt::Semi(expr, _) => {
+            todo!()
+        }
+    });
+    stmts
 }
